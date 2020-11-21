516313c282bd320924d90cf390663e6003810b73
hosam mohamed <hosamelmalt@gmail.com>
2020-11-21 15:28:16 -0800

    Initial Commit

---
+++ Crt App/Crt_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crt_AppApp.swift
+//  Crt App
+//
+//  Created by Hossam on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct Crt_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
